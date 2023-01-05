bcf43fd07144c0e28bda9db3053be0a0581dab8d
Corey K <121850504+Coreykwiat@users.noreply.github.com>
2023-01-05 18:03:31 -0500

    Add files via upload

--- peekaboo.py
+++ peekaboo.py
@@ -127,14 +127,19 @@ def LinkedIn():
     driver.find_element("id", "reset-password-submit-button").click()
     time.sleep(2.5)
     geturl = driver.current_url
-
-    if geturl == ("https://www.linkedin.com/uas/request-password-reset?trk=homepage-basic_signin-form_forgot-password-link"):
+    try:
+        if driver.find_element("xpath", "/html/body/div/div/div[1]"):
+            data[6] = 'LinkedIn Not Found Due To Captcha\n'
+        else:
+            if geturl == ("https://www.linkedin.com/uas/request-password-reset?trk=homepage-basic_signin-form_forgot-password-link"):
+                with open('output.txt', 'w') as f:
+                    data[6] = 'LinkedIn Account Not Found\n'
+            else:
+                with open('output.txt', 'w') as f:
+                    data[6] = 'LinkedIn Account Found\n'
+    except:
         with open('output.txt', 'w') as f:
-            data[6] = 'LinkedIn Account Not Found\n'
-    else:
-        with open('output.txt', 'w') as f:
-            data[6] = 'LinkedIn Account Found\n'
-
+            data[6] = 'No LinkedIn Account Found\n'
 LinkedIn()
 with open('output.txt', 'w') as file:
     file.writelines(data)
@@ -144,7 +149,7 @@ def TikTok():
     driver.get("https://www.tiktok.com/login/email/forget-password")
     try:
         driver.find_element("name", "email").send_keys(username)
-        driver.find_element("xpath", '//button[text()="Send code"]').click()
+        driver.find_element("xpath", '/html/body/div[2]/div/div[2]/div/form/div[3]/div/button').click()
         time.sleep(4)
         try:
             element = driver.find_element("xpath","//*[@type='text' and contains(text(), 'Email address isn't registered yet')]")
